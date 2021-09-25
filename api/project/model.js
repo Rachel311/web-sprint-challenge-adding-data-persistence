406afe45c70994983aa5099cef485f213aa6df90
Tory Rachel Van Tassel <countrygirlr09@hotmail.com>
2021-09-25 13:37:49 -0600

    completed migrations and started api's

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,35 @@
-// build your `Project` model here
+const db = require('.../data/dbConfig');
+
+const getProject = async () => {
+    const projects = await db('projects');
+
+    const projectMap = projects.map((project) => {
+        return {
+            project_id: project.project_id,
+            project_name: project.project_name,
+            project_description: project.project_description,
+            project_completed: project.project_completed === 0 ? false : true,
+        };
+    });
+    return projectMap
+};
+
+const getBy = async (id) => {
+    const projectFirst = await db('projects').where('project_id', id).first();
+    return {
+        project_id: projectFirst.project_id,
+        project_name: projectFirst.project_name,
+        project_description: projectFirst.project_description,
+        project_completed: projectFirst.project_completed === 0 ? false : true,
+    };
+};
+
+const createProject = async (project) => {
+    const [project_id] = await db('projects').insert(project);
+    return getBy(project_id);
+};
+
+module.exports = {
+    getProject,
+    createProject
+};
