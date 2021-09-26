b28f416194c6a967739410aaa536b90b763f4ced
Tory Rachel Van Tassel <countrygirlr09@hotmail.com>
2021-09-25 21:53:22 -0600

    completed routers/models

--- api/server.js
+++ api/server.js
@@ -13,4 +13,19 @@ server.use('/api/projects', projectRouter)
 server.use('/api/resources', resourceRouter)
 server.use('/api/tasks', taskRouter);
 
+server.get('/', (req, res) => {
+    res.json({
+        message: 'Welcome to the Sprint Challenge!'
+    });
+});
+
+//***********************500 error middleware***********//
+//eslint-disable-next-line
+server.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+      message: err.message,
+      devMessage: 'Something bad inside the server!'
+    });
+  });
+
 module.exports = server
--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,22 @@
-// build your `Task` model here
+const db = require('../../data/dbConfig');
+
+const getTask = async () => {
+    const allTasks = await db('tasks').leftJoin(
+        'projects',
+        'tasks.project_id',
+        'projects.project_id'
+    );
+
+    const tasks = allTasks.map((task) => {
+        return {
+            task_id: task.task_id,
+            task_notes: task.task_notes,
+            task_description: task.task_description,
+            task_completed: task.task_completed,
+            project_id: task.project_id,
+            project_name: task.project_name,
+            project_description: task.project_description,
+        };
+    });
+    return tasks;
+};
